233309360198f29f0e58778f302a5a539b8ef2e1
jose.fajardo <fajardo_jf@hotmail.com>
2017-12-27 17:05:22 +1100

    allow setting domain dn from commandline param

--- opendj/ldap/dev/Run-OpenDJ.bat
+++ opendj/ldap/dev/Run-OpenDJ.bat
@@ -1,9 +1,9 @@
 SET ROOT_USER=%1
 SET ROOT_USER_PASSWORD=%2
-rem SET BASE_DN=%3
 SET LDAP_PORT=%3
 SET ADMIN_PORT=%4
+SET BASE_DN=%5
 
-call c:\ldap\dev\Init-Container.bat %ROOT_USER% %ROOT_USER_PASSWORD% %LDAP_PORT% %ADMIN_PORT%
+call c:\ldap\dev\Init-Container.bat %ROOT_USER% %ROOT_USER_PASSWORD% %LDAP_PORT% %ADMIN_PORT% %BASE_DN%
 
 CALL PowerShell c:\ldap\dev\wait-service.ps1 -ServiceName "TimeBrokerSvc"
